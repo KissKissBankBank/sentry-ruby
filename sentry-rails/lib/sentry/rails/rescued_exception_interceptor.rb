89757593162a13f974408344a09a3cbc542863e5
William Pollet <william.pollet@kisskissbankbank.com>
2021-11-04 15:39:38 +0100

    rails 4 compat

--- sentry-rails/lib/sentry/rails/rescued_exception_interceptor.rb
+++ sentry-rails/lib/sentry/rails/rescued_exception_interceptor.rb
@@ -15,16 +15,14 @@ module Sentry
 
           # Rails' ShowExceptions#render_exception will mutate env for the exceptions app
           # so we need to hold a copy of env to report the accurate data (like request's url)
-          if request.show_exceptions?
-            scope = Sentry.get_current_scope
-            copied_env = scope.rack_env.dup
-            copied_env["sentry.original_transaction"] = scope.transaction_name
-            scope.set_rack_env(copied_env)
+          scope = Sentry.get_current_scope
+          copied_env = scope.rack_env.dup
+          copied_env["sentry.original_transaction"] = scope.transaction_name
+          scope.set_rack_env(copied_env)
 
-            if report_rescued_exceptions?
-              Sentry::Rails.capture_exception(e)
-              env["sentry.already_captured"] = true
-            end
+          if report_rescued_exceptions?
+            Sentry::Rails.capture_exception(e)
+            env["sentry.already_captured"] = true
           end
 
           env["sentry.rescued_exception"] = e if report_rescued_exceptions?
